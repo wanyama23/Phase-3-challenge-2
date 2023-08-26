49eb08e78f9b97ddb2060fa63680c0932e7ea592
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-26 23:43:21 +0300

    initislizing customer

---
+++ customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
+        Customer.all_customers.append(self)
