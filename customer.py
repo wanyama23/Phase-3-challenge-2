3b21f852fc0ada60b1ab7136a47f76d1d66c2766
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-27 22:46:55 +0300

    customer

--- customer.py
+++ customer.py
@@ -30,4 +30,34 @@ class Customer:
     
     def full_name(self):
         return f"{self.given_name} {self.family_name}"
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+    
+    def add_review(self,restaurant,rating):
+        review  = Review(self,restaurant,rating)
+        self.reviews.append(review)
+        restaurant.add_review(review)
+        
+    def num_reviews(self):
+        return len(self.reviews)
+    
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            if customer.full_name() == name:
+                return customer
+        return None
+    
+    @classmethod
+    def find_all_by_given_name(cls,name):
+        customers = []
+        for customer in cls.all_customers:
+         if customer.given_name()==name:
+            customers.append = customers
+            return customers
+
+
+customer1 = Customer("mary", "John") 
+# print(Customer)          
     
