2000a5f6a503c8ac43670e6f88ba4d2fe95ac11a
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-28 00:06:33 +0300

    restaurant

--- restaurant.py
+++ restaurant.py
@@ -2,3 +2,7 @@ class Restaurant:
     
     def __init__(self, name):
         self.name = name
+        self.reviews = []
+        
+    def add_review(self, rating):
+        self.reviews.append(rating)
