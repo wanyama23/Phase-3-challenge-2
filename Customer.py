94453dd8c19aaede5755c83309c948ec6595a5d3
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-28 12:24:36 +0300

    updating customer

--- Customer.py
+++ Customer.py
@@ -1,12 +1,4 @@
-class Customer:
-    all_customers = []
-
-    def __init__(self, given_name, family_name):
-        self.given_name = given_name
-        self.family_name = family_name
-        self.reviews = []
-        Customer.all_customers.append(self)
-
+import Review
 class Customer:
     all_customers = []
     
@@ -59,5 +51,5 @@ class Customer:
 
 
 customer1 = Customer("mary", "John") 
-# print(Customer)          
+print(customer1)          
     
